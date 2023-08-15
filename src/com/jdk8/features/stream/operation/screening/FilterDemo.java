bb9634bea796b7701b167bf01017f36410fb2ce3
yujale98 <yujale98@gmail.com>
2023-08-15 21:55:43 +0800

    feat: Stream 筛选操作的 filter 过滤基本实现

--- src/com/jdk8/features/stream/operation/screening/FilterDemo.java
+++ src/com/jdk8/features/stream/operation/screening/FilterDemo.java
@@ -7,16 +7,11 @@ import java.util.stream.Collectors;
 public class FilterDemo {
 
   public static void main(String[] args) {
-    ArrayList<String> strings = new ArrayList<>();
-    strings.add("a");
-    strings.add("b");
-    strings.add("c");
-    strings.add("d");
-    strings.add("e");
-
-    List<String> a = strings.stream().filter(s -> s.equals("a")).collect(Collectors.toList());
-
-    System.out.println(a.toString());
+    List<String> words = List.of("apple", "banana", "cherry", "date", "elderberry");
+    List<String> longWords = words.stream()
+        .filter(word -> word.length() > 5)
+        .collect(Collectors.toList());
+    System.out.println("Long words: " + longWords);
   }
 
 }

cdeaf8fdb094ebae444245fa64a57f43eff51133
yujale98 <yujale98@gmail.com>
2023-08-15 21:57:14 +0800

    feat: Stream 筛选操作的 distinct 基本使用

--- src/com/jdk8/features/stream/operation/screening/DistinctDemo.java
+++ src/com/jdk8/features/stream/operation/screening/DistinctDemo.java
@@ -2,19 +2,17 @@ package com.jdk8.features.stream.operation.screening;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DistinctDemo {
 
   public static void main(String[] args) {
 
-  }
+    List<Integer> numbers = List.of(1, 2, 2, 3, 4, 4, 5, 5, 5);
 
-  public static List<Integer> demo1(List<Integer> list) {
-
-
-    return null;
-
-
-
+    List<Integer> distinctNumbers = numbers.stream()
+        .distinct()
+        .collect(Collectors.toList());
+    System.out.println("Distinct numbers: " + distinctNumbers);
   }
 }
